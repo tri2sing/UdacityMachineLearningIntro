a4313270d150310aad5c7388e1da70db219fed3d
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-06-18 08:50:29 -0700

    answered a few more questions

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,14 +22,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
 #########################################################
-clf = SVC(kernel='linear')
-t0 = time()
-clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
-t1 = time()
-labels_pred = clf.predict(features_test)
-print "prediction time:", round(time()-t1, 3), "s"
-accuracy = accuracy_score(labels_pred, labels_test)
-print accuracy
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+#clf = SVC(kernel='linear')
+for C in [10, 100, 1000, 10000]:
+    clf = SVC(kernel='rbf', C=C)
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "C = ", C,
+    print "training time:", round(time()-t0, 3), "s",
+    t1 = time()
+    labels_pred = clf.predict(features_test)
+    print "prediction time:", round(time()-t1, 3), "s",
+    accuracy = accuracy_score(labels_pred, labels_test)
+    print "accuracy = ", accuracy
