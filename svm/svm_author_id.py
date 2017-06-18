5dde5ecfcb47a5dd13cd6b519093dcb37c4778e1
Sameer Adhikari <sameer.adhikari@oracle.com>
2017-06-17 22:52:44 -0700

    completed svm linear kernel accuracy question

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
 #########################################################
-
-
+clf = SVC(kernel='linear')
+clf.fit(features_train, labels_train)
+labels_pred = clf.predict(features_test)
+accuracy = accuracy_score(labels_pred, labels_test)
+print accuracy
